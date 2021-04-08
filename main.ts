96c3f0e7c978ab505888f353bd56aed4ecc6400b
TreyyyT <82166923+TreyyyT@users.noreply.github.com>
2021-04-08 08:36:25 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,79 @@
-
+namespace SpriteKind {
+    export const Arrow = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . 7 . . . 
+        . . . 7 7 7 7 7 7 7 7 7 7 7 . . 
+        . . . . . . . . . . . . 7 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, spaceShip, 50, 0)
+    projectile.setKind(SpriteKind.Arrow)
+})
+sprites.onOverlap(SpriteKind.Arrow, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy()
+})
+let alien: Sprite = null
+let projectile: Sprite = null
+let spaceShip: Sprite = null
+spaceShip = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 1 1 1 1 . . . . . . 
+    . . . . . 1 1 1 1 1 1 . . . . . 
+    . . . 8 1 1 1 1 1 1 1 1 8 . . . 
+    . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(spaceShip, 200, 200)
+spaceShip.setStayInScreen(true)
+info.setLife(3)
+game.onUpdateInterval(500, function () {
+    alien = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . c c . . . . . . . . . . . 
+        . . . . a a . . . . . . a c c . 
+        . . . . . a . . . . . a a . . . 
+        . . . . . . a a a a a a . . . . 
+        . . . . a a a b a a b a a . . . 
+        . . . . a a a a a a a a a . . . 
+        . . . a a a a b b b b a a a . . 
+        . . . a a a a a a a a a a a . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    alien.setVelocity(-50, 0)
+    alien.setPosition(160, randint(10, 120))
+})
