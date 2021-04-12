e3259c117db475e8f37768797aa17cb1d5cfeb61
TreyyyT <82166923+TreyyyT@users.noreply.github.com>
2021-04-12 15:31:21 -0700

    finished

--- main.ts
+++ main.ts
@@ -25,15 +25,24 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Arrow, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprite.destroy()
     otherSprite.destroy(effects.fire, 500)
+    music.zapped.play()
     info.changeScoreBy(1)
+})
+info.onLifeZero(function () {
+    music.bigCrash.play()
+    game.splash("SAD.")
+    game.reset()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
-    otherSprite.destroy()
+    otherSprite.destroy(effects.ashes, 500)
+    music.smallCrash.play()
 })
 let alien: Sprite = null
+let alien2: Sprite = null
 let projectile: Sprite = null
 let spaceShip: Sprite = null
+music.setVolume(10)
 spaceShip = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -55,6 +64,31 @@ spaceShip = sprites.create(img`
 controller.moveSprite(spaceShip, 200, 200)
 spaceShip.setStayInScreen(true)
 info.setLife(3)
+game.onUpdateInterval(2000, function () {
+    alien2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . 4 4 . 
+        . . . . . . . . . . . . 4 4 4 . 
+        . 4 4 4 1 4 4 4 . . . 4 1 4 . . 
+        4 f 4 4 1 4 4 4 1 . 4 4 4 1 . . 
+        4 4 4 4 4 1 4 4 1 4 4 4 4 . . . 
+        4 4 4 4 4 1 4 4 4 1 4 4 4 . . . 
+        4 f 4 4 1 4 4 4 1 4 4 4 4 4 . . 
+        f 4 4 4 1 4 4 4 1 . . 4 4 1 . . 
+        . 4 4 4 1 4 4 4 . . . . 1 4 4 . 
+        . . . . . . . . . . . . . 4 4 . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    alien2.setVelocity(-75, 0)
+    alien2.setPosition(160, randint(10, 120))
+})
+forever(function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
 game.onUpdateInterval(500, function () {
     alien = sprites.create(img`
         . . . . . . . . . . . . . . . . 
